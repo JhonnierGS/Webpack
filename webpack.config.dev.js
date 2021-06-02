c28632d1455f49912cc6632ea6eaa7a39bdc8c2d
jhonnieGS <gs.170590@gmail.com>
2021-06-02 18:30:38 -0500

    add devServer and devTools

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,7 +13,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     mode: 'development',
-    watch: true,
+    devtool: 'source-map',
     resolve:{
         extensions: ['.js'],
         alias: {
@@ -78,4 +78,10 @@ module.exports = {
         }),
         new Dotenv(),
     ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+    },
 }
